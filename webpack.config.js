2afea492f24a1edba078443b2cfb5f7ff6b12155
Artem Grachov <grachov2195@gmail.com>
2017-10-04 16:02:22 +0300

    Link to JS and CSS bundle in page template

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,14 @@ const sass = require('./webpack/sass');
 const babel = require('./webpack/babel');
 const pug = require('./webpack/pug');
 const img = require('./webpack/img');
+const server = require('./webpack/server');
 
 const PATHS = {
     app: path.resolve(__dirname, 'app'),
     dist: path.resolve(__dirname, 'dist')
 }
 
-module.exports = merge([{
+const common = merge([{
         context: PATHS.app,
         entry: './js/main.js',
         output: {
@@ -29,3 +30,24 @@ module.exports = merge([{
     pug(),
     img()
 ])
+
+module.exports = function (env) {
+    switch (env) {
+        case 'prod':
+            return merge([
+                common
+            ])
+            break;
+        case 'dev':
+            return merge([
+                common,
+                server()
+            ])
+            break;
+        case 'serv':
+            return merge([
+                common
+            ])
+            break;
+    }
+}
